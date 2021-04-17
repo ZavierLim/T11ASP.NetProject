37156b6660bc166ed43fe89cd7eaef07e0a011fa
HG <harrison28590@gmail.com>
2021-04-17 12:04:48 +0800

    1. Added the jQuery search removal functionality on product gallery page

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$("#search").bind("input propertychange", function () {
+    var value = $(this).val();
+    if (!value) {
+
+        window.location.href = "./Home/Index";
+    }
+});
